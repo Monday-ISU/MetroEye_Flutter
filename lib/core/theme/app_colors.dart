40ad0cfd59c42bbbdf02d6140ac0ecdfdc6f52a4
june0103 <rhwns0103@naver.com>
2026-04-05 21:56:25 +0900

    Chore: app color 상수 네이밍 정리 #2

--- lib/core/theme/app_colors.dart
+++ lib/core/theme/app_colors.dart
@@ -1,7 +1,6 @@
 import 'package:flutter/material.dart';
 
 abstract final class AppColors {
-
   static const gray1 = Color(0xFFF8F8F9);
   static const gray2 = Color(0xFFE8E8EA);
   static const gray3 = Color(0xFFD2D1D4);
@@ -9,11 +8,10 @@ abstract final class AppColors {
   static const gray5 = Color(0xFF2D2836);
   static const gray6 = Color(0xFF1D1D1D);
 
-  static const sub_gray1 = Color(0xFFF0F0F2);
-  static const sub_gray2 = Color(0xFFD9D9D9);
-  static const sub_gray3 = Color(0xFF545454);
+  static const subGray1 = Color(0xFFF0F0F2);
+  static const subGray2 = Color(0xFFD9D9D9);
+  static const subGray3 = Color(0xFF545454);
 
   static const no = Color(0xFFF0743E);
   static const warning = Color(0xFFFDD849);
-
 }
